fdff88d97e3e550bcae019a5f7e004e14a9c9ddb
pmadan775 <pmadan775@gmail.com>
2021-08-23 20:14:06 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,83 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles2 = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+      marginTop: "100px",
+          
+          
+
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+ function SimpleAccordion() {
+  const classes = useStyles2();
+
+  return (
+    <div className={classes.root} my={5}>
+     <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="sm">
+       <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography className={classes.heading}>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
+
+    </Container>
+
+    </React.Fragment>
+   
+    </div>
+  );
+}
+ReactDOM.render(< SimpleAccordion/>, document.querySelector('#root6'));
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
