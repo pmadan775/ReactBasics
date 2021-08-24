39f39167dd8908ac63bdc88939f5fc42b1a6781f
pmadan775 <pmadan775@gmail.com>
2021-08-24 20:28:42 +0530

    third Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import FixedContainer from'./App.js';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -14,11 +13,255 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+
+const useStyles6= makeStyles((theme) => ({
+  root: {
+    minWidth: 275,
+    margin: `${theme.spacing(2)}px auto`,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}));
+
+ function SimpleCard() {
+  const classes = useStyles6();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+ReactDOM.render(<SimpleCard/>, document.querySelector("#root10"));
+
+
+const useStyles5= makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(3),
+      
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  leftSpace:{
+    marginRight: theme.spacing(3),
+  }
+}));
+
+ function FloatingActionButtons() {
+  const classes = useStyles5();
+
+  return (
+
+    <div className={classes.root}>
+      <Grid container justify = "center">
+      <Fab className={classes.leftSpace} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab className={classes.leftSpace} color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab className={classes.leftSpace} variant="extended">
+        <NavigationIcon className={classes.extendedIcon} />
+        Navigate
+      </Fab>
+      <Fab  disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+      </Grid>
+      
+    </div>
+  );
+}
+ReactDOM.render(<FloatingActionButtons/>, document.querySelector("#root9"));
+
+
+const useStyles4 = makeStyles((theme) => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3),
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
+
+function CSSGrid() {
+  const classes = useStyles4();
+
+  return (
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="md">
+    <div>
+      <Typography variant="subtitle1" gutterBottom>
+        Material-UI Grid:
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>xs=4</Paper>
+        </Grid>
+      </Grid>
+      <Divider className={classes.divider} />
+      <Typography variant="subtitle1" gutterBottom>
+        CSS Grid Layout:
+      </Typography>
+      <div className={classes.container}>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 3' }}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 8' }}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </div>
+        <div style={{ gridColumnEnd: 'span 4' }}>
+          <Paper className={classes.paper}>xs=4</Paper>
+        </div>
+      </div>
+    </div>
+
+      </Container>
+      </React.Fragment>
+    
+  );
+}
+ReactDOM.render(<CSSGrid/>,document.querySelector("#root8"));
+
+
+const useStyles3 = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(10, 10,5,5),
+  },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing(2)}px auto`,
+    padding: theme.spacing(1),
+  },
+}));
+
+const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
+
+function AutoGridNoWrap() {
+  const classes = useStyles3();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <Typography noWrap>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography noWrap>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography>{message}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+ReactDOM.render(<AutoGridNoWrap/>,document.querySelector("#root7"));
+
 
 const useStyles2 = makeStyles((theme) => ({
   root: {
     width: '100%',
-      marginTop: "100px",
+    marginTop: "100px",
           
           
 
@@ -245,14 +488,7 @@ function App() {
 }
 ReactDOM.render(<App/>,document.getElementById('root1'));
 
-function box(){
-  return(
 
-    <Box component="span" m={1}>
-      {'Hello'}
-    </Box>
-  );
-}
 
 
 function SimpleContainer() {
